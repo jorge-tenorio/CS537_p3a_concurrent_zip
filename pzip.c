bf11371172db5f8bdf60b9227f889c60ce253986
Lukas Wehrli <wehrli@vm-instunix-11.cs.wisc.edu>
2021-11-04 17:43:20 -0500

    Able to read file now

--- pzip.c
+++ pzip.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
-int main(int argc, char *args[]){
-	if(argc < 1){
+int main(int argc, char *argv[]){
+	if(argc < 2){
 		printf("File not specified\n");
 		exit(1);
 	}
 	
 	// read all the files given to us
-	for(int i = 0; i < argc; i++){
-		FILE *file = fopen(args[i], "r");
+	for(int i = 1; i < argc; i++){
 		
-				
+		char *f;
+		int size;
+		struct stat s;
 
-		fclose(file);
+		int file = open(argv[i], O_RDONLY);
+		fstat (file, & s);
+		size = s.st_size;
+		
+		f = (char *) mmap (0, size, PROT_READ, MAP_PRIVATE, file, 0);
+
+		for (int i = 0; i < size; i++) {
+			char c;
+
+			c = f[i];
+			putchar(c);
+		}
+		return 0;
 	}
 }
