330611fa064b9a42cbf39625ac60e4a9fa5c92af
Lukas Wehrli <wehrli@vm-instunix-02.cs.wisc.edu>
2021-11-04 17:17:40 -0500

    Reading file

--- pzip.c
+++ pzip.c
@@ -6,4 +6,13 @@ int main(int argc, char *args[]){
 		printf("File not specified\n");
 		exit(1);
 	}
+	
+	// read all the files given to us
+	for(int i = 0; i < argc; i++){
+		FILE *file = fopen(args[i], "r");
+		
+				
+
+		fclose(file);
+	}
 }
