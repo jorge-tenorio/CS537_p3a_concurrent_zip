32d0206319b73239ceb0caf828e75890d89b64ee
Jorge Tenorio <tenorio@snares-04.cs.wisc.edu>
2021-11-04 18:23:34 -0500

    changes

--- pzip.c
+++ pzip.c
@@ -22,12 +22,21 @@ int main(int argc, char *argv[]){
 		size = s.st_size;
 		
 		f = (char *) mmap (0, size, PROT_READ, MAP_PRIVATE, file, 0);
-
+		char prevChar = f[0];
+		int charCount = 1;
 		for (int i = 0; i < size; i++) {
-			char c;
-
-			c = f[i];
-			putchar(c);
+			char currChar;
+			currChar = f[i];
+			if (strcmp(currChar, '\n') == 0) {
+				printf("\n");
+			} else if (strcmp(currChar, prevChar) == 0) {
+				charCount++;
+			} else {
+				printf(%i%d,charCount, prevChar);
+				prevChar = currChar;
+				charCount = 1;
+			}
+			//putchar(c);
 		}
 		return 0;
 	}
