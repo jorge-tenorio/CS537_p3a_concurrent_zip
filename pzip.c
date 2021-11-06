26aa4a190e7533565a6f11914409a9b7e35fdb8e
Jorge Tenorio <tenorio@royal-03.cs.wisc.edu>
2021-11-06 13:37:17 -0500

    added \0 correction, added address comparison, added null test

--- pzip.c
+++ pzip.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[]){
 		printf("File not specified\n");
 		exit(1);
 	}
-	
+
 	// read all the files given to us
 	for(int i = 1; i < argc; i++){
-		
+
 		char *f;
 		int size;
 		struct stat s;
@@ -20,20 +20,31 @@ int main(int argc, char *argv[]){
 		int file = open(argv[i], O_RDONLY);
 		fstat (file, & s);
 		size = s.st_size;
-		
+
 		f = (char *) mmap (0, size, PROT_READ, MAP_PRIVATE, file, 0);
 		char prevChar = f[0];
 		int charCount = 1;
 		for (int i = 0; i < size; i++) {
 			char currChar;
 			currChar = f[i];
-			//if (strcmp(currChar, '\n') == 0) {
-			//	printf("\n");
-			//} else 
+
+			// Skip \0, do not reset char count
+			if (currChar == 00) continue;
+
+			// Address compare
+			//if (i > 0) {
+			//	printf("\naddress jump: [%d]: %p > [%d]: %p\n,", i-1, &f[i-1], i, &f[i]);
+			//}
+
+			// Main zip function 
 			if (currChar == prevChar) {
 				charCount++;
 			} else {
-				printf("%d%c",charCount, prevChar);
+				if (prevChar == 10) {
+					printf("%d/n",charCount);
+				} else {
+					printf("%d%c",charCount, prevChar);
+				}
 				prevChar = currChar;
 				charCount = 1;
 			}
